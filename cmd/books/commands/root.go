0f16775992a4cc1e9b6d1c231399948b17f22542
Tyler Spivey <tspivey@pcdesk.net>
2018-04-04 10:18:19 -0700

    Improve config handling

--- cmd/books/commands/root.go
+++ cmd/books/commands/root.go
@@ -71,7 +71,8 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading config file: %s\n", err)
+		os.Exit(1)
 	}
 }
